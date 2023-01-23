ba19809038aac3f99269f05fa62a1ff7649ddc71
JZenon13 <jz.the.dev@gmail.com>
2023-01-23 09:52:15 -0600

    schema

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -3,14 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import * as Joi from "joi"
+import { DatabaseModule } from '@app/common';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     validationSchema: Joi.object({
       MONGODB_URI: Joi.string()
-    })
-  })],
+    }),
+    envFilePath: process.env.MONGOBD_URI
+  }),
+  DatabaseModule
+],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
